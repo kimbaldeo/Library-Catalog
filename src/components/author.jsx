3ab87c581f1b10f9e6fab3dcd701f0ef64008006
kbaldeoward <kbaldeoward@gmail.com>
2022-05-17 11:02:42 -0400

    author page progress

--- src/components/author.jsx
+++ src/components/author.jsx
@@ -1,4 +1,20 @@
+import config from './config.json';
+
 function Author() {
+    const fetchAuthorURL = `https://books.googleapis.com/books/v1/volumes?q=""&printType=books&orderBy=newest&maxResults=40&key=${config.apiKey}`
+    const [author, setAuthor] = useState("")
+
+    function getAuthor() {
+        const url = fetchAuthorURL;
+        fetch(url)
+        .then((res) =>  res.json())
+        .then((res) =>  setBooks((res.items)))
+    }
+
+    useEffect(() => {
+        getAuthor()
+    }, [])
+
     return (
         <h1>author page</h1>   
     )
