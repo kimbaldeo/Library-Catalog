0aa1f7934b29d6eea26faeade5ccc8d09dfa6489
kbaldeoward <kbaldeoward@gmail.com>
2022-05-20 01:42:50 -0400

    data

--- frontend/src/pages/bookShelf.jsx
+++ frontend/src/pages/bookShelf.jsx
@@ -1,17 +1,23 @@
-// import React from 'react';
-// function Bookshelf() {
-//     return (
-//         <h1>My Bookshelf</h1>
-//         (props.books.map((book, idx) => (
-//             <div key = { idx } className = "bookView">
-//                 <Link to = {`/shelf/${idx}`}>
-//                     <img src = {book.volumeInfo.imageLinks.smallThumbnail} className = "allbookscover" alt = "book cover"/>
-//                     <p>{book.volumeInfo.title}</p>
-//                 </Link>
-//             </div>
-//         ))
-//         ) : (<div>Loading...</div>)
-//     )
-// }
+import React from 'react';
+function Bookshelf() {
+    return (
+        <>
+        <h1>My Bookshelf</h1>
+        <div className = "bookView">
+            <
+        </div>
 
-// export default Bookshelf
+        </>
+        // (props.books.map((book, idx) => (
+        //     <div key = { idx } className = "bookView">
+        //         <Link to = {`/shelf/${idx}`}>
+        //             <img src = {book.volumeInfo.imageLinks.smallThumbnail} className = "allbookscover" alt = "book cover"/>
+        //             <p>{book.volumeInfo.title}</p>
+        //         </Link>
+        //     </div>
+        // ))
+        // ) : (<div>Loading...</div>)
+    )
+}
+
+export default Bookshelf
